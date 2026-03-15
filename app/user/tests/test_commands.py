dfcb85dd8cf083cd97e57f0a89b9c9bdc1ec34c0
Borja <borjagarciavillanueva@gmail.com>
2026-03-16 00:10:58 +0100

    refactor of the project, fix some dev containers config and fixed tests

--- app/user/tests/test_commands.py
+++ app/user/tests/test_commands.py
@@ -1,16 +1,16 @@
 """
 Test custom Django management commands.
 """
-from unittest.mock import patch
-
-from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase
+from django.test import TestCase
+from psycopg2 import OperationalError as Psycopg2OpError
+from unittest.mock import patch
 
-@patch('core.management.commands.wait_for_db.Command.check')
-class CommandTests(SimpleTestCase):
+
+
+@patch('user.management.commands.wait_for_db.Command.check')
+class CommandTests(TestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -19,7 +19,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(database='default')
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -29,4 +29,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(database='default')

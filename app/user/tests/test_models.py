50787a34fee50e53d4af60f9f2b2a14537e1baf4
Borja García Villanueva <borja@MacBook-Pro-de-Borja.local>
2026-03-18 20:51:37 +0100

    Created user model and migration with tests

--- app/user/tests/test_models.py
+++ app/user/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Tests for the models in the core app.
+Tests for the models in the user app.
 """
 
 from django.contrib.auth import get_user_model
@@ -19,3 +19,30 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email its normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@EXAMPLE.COM', 'test4@example.com'],
+        ]
+        
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raise_error(self):
+        """Test if user has no email raises an error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test')
+
+    def test_create_superuser(self):
+        """Test create a superuser"""
+        superuser = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test'
+        )
+        self.assertTrue(superuser.is_superuser)
+        self.assertTrue(superuser.is_staff)

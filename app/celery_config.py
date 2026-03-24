09eb3ae61776b55a9eaad5a45ba783bf22c9d3c9
Borja García Villanueva <borja@MacBook-Pro-de-Borja.local>
2026-03-24 19:39:46 +0100

    cron job for celery for update_bgg_catalog task

--- app/celery_config.py
+++ app/celery_config.py
@@ -4,6 +4,8 @@ Config for celery and Cron jobs
 
 import os
 
+from celery.schedules import crontab
+
 # Broker & Backend
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
@@ -15,3 +17,13 @@ CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'UTC'
 CELERY_TASK_SOFT_TIME_LIMIT = 300
 CELERY_RESULT_EXPIRES = 3600
+
+# Cron Jobs (Beat Schedule)
+
+CELERY_BEAT_SCHEDULE = {
+    'import-bgg-games-weekly': {
+        'task': 'tasks.update_games_catalog',
+        'schedule': crontab(minute=0, hour=4, day_of_week='friday'),
+        'args': (5,),
+    },
+}

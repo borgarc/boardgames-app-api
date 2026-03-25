f7e15ad974f6309406ee3f96c6d129c03fbd532c
Borja García Villanueva <borja@MacBook-Pro-de-Borja.local>
2026-03-25 18:35:49 +0100

    refactor batch_count this wont be sent as argument will be on the BGGCatalogRepository update_catalog method

--- game/management/commands/sync_test.py
+++ game/management/commands/sync_test.py
@@ -6,21 +6,9 @@ from game.tasks import update_bgg_catalog
 class Command(BaseCommand):
     help = 'Starts board game synchronization from BGG.'
 
-    def add_arguments(self, parser):
-        # Allows passing the number of batches as an optional argument.
-        parser.add_argument(
-            'batches', 
-            type=int, 
-            nargs='?', 
-            default=1, 
-            help='Number of 20-game batches to download (default: 1).'
-        )
-
     def handle(self, *args, **options):
-        batches = options['batches']
-        self.stdout.write(self.style.SUCCESS(f'Starting download of {batches * 20} games...'))
+        self.stdout.write(self.style.SUCCESS('Starting downloading games...'))
         
-        # Executing the function directly (synchronously) to monitor progress in the console.
-        update_bgg_catalog(batch_count=batches)
+        update_bgg_catalog()
         
         self.stdout.write(self.style.SUCCESS('Synchronization completed successfully!'))
